8e2fea402f27e162956d2f387f46fa858d10910a
oldrover <oldrover@web.de>
2021-05-21 13:05:00 +0200

    created imageprocessor.ts

---
+++ src/utilities/imageprocessor.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import sharp from 'sharp';
+
+const imageprocessing = (
+    req: express.Request, 
+    res: express.Response, 
+    next: Function) => {
+
+}
+
+export default imageprocessing;
