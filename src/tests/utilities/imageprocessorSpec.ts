86e0620426802e8e0c2d0cae1fe52b1f67fca868
oldrover <oldrover@web.de>
2021-06-01 13:45:29 +0200

    added imageprocessing tests

---
+++ src/tests/utilities/imageprocessorSpec.ts
@@ -0,0 +1,20 @@
+import imageprocessor from '../../utilities/imageprocessor';
+import ImageFile from '../../utilities/imageFileClass';
+import fs from 'fs';
+
+const imageFile = new ImageFile('fjord', '300', '300');
+
+describe('imageprocessor unit test', () => {
+    it('should process an image, save it and return true', () => {
+
+        return imageprocessor.processFile(imageFile).then( result => {
+            expect(result).toBeTrue();
+    
+        });
+    });
+    it('processed image should be there', () => {
+        expect(fs.existsSync(imageFile.getOutPath())).toBeTrue();
+
+    })
+});
+
