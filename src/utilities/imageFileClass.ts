012d884f3d9beaa99e316a818520251a441a652d
oldrover <oldrover@web.de>
2021-06-01 13:44:26 +0200

    lint changes

--- src/utilities/imageFileClass.ts
+++ src/utilities/imageFileClass.ts
@@ -1,35 +1,34 @@
+/* eslint-disable require-jsdoc */
 const SRC_PATH = './images/full/';
 const OUT_PATH = './images/thumb/';
 
 class ImageFile {
-    name: string;
-    width: string;
-    height: string;
-    #srcPath: string;    
-    #outPath: string;
+  name: string;
+  width: string;
+  height: string;
+  #srcPath: string;
+  #outPath: string;
 
-    constructor(name: string, width: string, height: string) {
-        this.name = name;
-        this.width = width;
-        this.height = height;
-        this.#srcPath = SRC_PATH + this.name + '.jpg';
-        this.#outPath = OUT_PATH + this.name + this.width + 'x' + this.height + '.jpg';
-              
-    }
-   
-    getOutPath () {
-        return this.#outPath;
-    }
+  constructor(name: string, width: string, height: string) {
+    this.name = name;
+    this.width = width;
+    this.height = height;
+    this.#srcPath = SRC_PATH + this.name + '.jpg';
+    this.#outPath =
+      OUT_PATH + this.name + this.width + 'x' + this.height + '.jpg';
+  }
 
-    getSrcPath () {
-        return this.#srcPath;
-    }
+  getOutPath(): string {
+    return this.#outPath;
+  }
 
-    getRoutePath() {
-        return this.name + this.width + 'x' + this.height + '.jpg';
-    }
-    
+  getSrcPath(): string {
+    return this.#srcPath;
+  }
 
+  getRoutePath(): string {
+    return this.name + this.width + 'x' + this.height + '.jpg';
+  }
 }
 
 export default ImageFile;
