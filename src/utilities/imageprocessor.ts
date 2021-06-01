e4bf03a6cfdf08f0639b2addb5b8eee85279c0d5
oldrover <oldrover@web.de>
2021-06-01 13:45:18 +0200

    formatting

--- src/utilities/imageprocessor.ts
+++ src/utilities/imageprocessor.ts
@@ -1,60 +1,55 @@
+/* eslint-disable @typescript-eslint/ban-types */
 import express from 'express';
-import fs, { PathLike } from 'fs';
+import fs from 'fs';
 import sharp from 'sharp';
 import ImageFile from './imageFileClass';
 
-
 const imageprocessing = async (
-    req: express.Request, 
-    res: express.Response, 
-    next: Function): Promise<void> => {
-        
-    if(!req.query.filename || !req.query.width || !req.query.height) {        
-        !req.query.filename ? res.send("Please add an image filename") 
-        : !req.query.width ? res.send("Please add a width value")
-        : res.send("Please add a height value")
-        
-    }      
-    else {
-        const imageFile = new ImageFile(req.query.filename as string, 
-            req.query.width as string, 
-            req.query.height as string);
+  req: express.Request,
+  res: express.Response,
+  next: Function
+): Promise<void> => {
+  if (!req.query.filename || !req.query.width || !req.query.height) {
+    !req.query.filename
+      ? res.send('Please add an image filename')
+      : !req.query.width
+      ? res.send('Please add a width value')
+      : res.send('Please add a height value');
+  } else {
+    const imageFile = new ImageFile(
+      req.query.filename as string,
+      req.query.width as string,
+      req.query.height as string
+    );
 
-        if(fs.existsSync(imageFile.getOutPath())) {            
-            res.send(`<img src=../thumb/${imageFile.getRoutePath()}>`);
+    if (fs.existsSync(imageFile.getOutPath())) {
+      res.send(`<img src=../thumb/${imageFile.getRoutePath()}>`);
+    } else {
+      if (fs.existsSync(imageFile.getSrcPath())) {
+        if (await processFile(imageFile)) {
+          res.send(`<img src=../thumb/${imageFile.getRoutePath()}>`);
+        } else {
+          res.send('Error occured while processing the image!');
         }
-        else {
-            if(fs.existsSync(imageFile.getSrcPath())) {
-
-
-                if( await processFile(imageFile)) {                     
-                    res.send(`<img src=../thumb/${imageFile.getRoutePath()}>`);
-                }
-                else {                    
-                    res.send("Error occured while processing the image!");    
-                }
-    
-            }          
-            else {
-                console.log(`requested file not found ${imageFile.getSrcPath()}`)
-                res.send('file not found');    
-            } 
-        }           
-    }       
-    next();
-}
+      } else {
+        console.log(`requested file not found ${imageFile.getSrcPath()}`);
+        res.send('file not found');
+      }
+    }
+  }
+  next();
+};
 
 const processFile = async (imageFile: ImageFile): Promise<boolean> => {
+  let check = false;
 
-    let check: boolean = false;
-        
-    await sharp(imageFile.getSrcPath())
-        .resize(parseInt(imageFile.width), parseInt(imageFile.height))
-        .toFile(imageFile.getOutPath())         
-        .then(()=> check = true)
-        .catch(()=> check = false)  
+  await sharp(imageFile.getSrcPath())
+    .resize(parseInt(imageFile.width), parseInt(imageFile.height))
+    .toFile(imageFile.getOutPath())
+    .then(() => (check = true))
+    .catch(() => (check = false));
 
-    return check;
-}
+  return check;
+};
 
-export default imageprocessing;
+export default { imageprocessing, processFile };
